11cb467d53a7a270f490f141937593e41965f42a
Juan Vásquez <javasgon@gmail.com>
2024-06-21 08:50:44 -0600

    fix(linter): Follow rubocop cops (#13)

--- app/helpers/simple_form/themes/application_helper.rb
+++ app/helpers/simple_form/themes/application_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module SimpleForm
   module Themes
     module ApplicationHelper
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
+# frozen_string_literal: true
+
 SimpleForm::Themes::Engine.routes.draw do
 end
